f78045c14b905e8d0e168f27b2cb9846cdfacf6f
Dumping ABI <laxajaxmail@gmail.com>
2017-12-19 18:20:05 +0800

    enum constructor

--- diancanrest/src/main/java/com/diancan/domain/resto/constants/DrinkCategory.java
+++ diancanrest/src/main/java/com/diancan/domain/resto/constants/DrinkCategory.java
@@ -1,7 +1,35 @@
 package com.diancan.domain.resto.constants;
 
+import antlr.StringUtils;
+
 public enum DrinkCategory {
-    COCO,
-    ALCOHOLIC,
-    NON_ALCOHOLIC
+
+    COCO(1,"coco"),
+    ALCOHOLIC(2,"alcoholic"),
+    NON_ALCOHOLIC(3,"no_alcoholic");
+
+    private  final Integer value;
+    private  final String desc;
+    DrinkCategory(Integer value ,String desc) {
+        this.value =value;
+        this.desc =desc;
+    }
+
+    public  int value() {
+        return  this.value;
+    }
+
+    public static DrinkCategory valueOf(int code) {
+        DrinkCategory[] var1 = values();
+        int var2 = var1.length;
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            DrinkCategory status = var1[var3];
+            if (status.value == code) {
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException("No matching constant for [" + code + "]");
+    }
 }
--- diancanrest/src/main/java/com/diancan/domain/resto/constants/DrinkTeste.java
+++ diancanrest/src/main/java/com/diancan/domain/resto/constants/DrinkTeste.java
@@ -2,6 +2,30 @@ package com.diancan.domain.resto.constants;
 
 //饮料口味
 public enum DrinkTeste {
-    SOFT,
-    HARD
+    SOFT(1,"soft"),
+    HARD(2,"hard");
+    private  final Integer value;
+    private  final String desc;
+    DrinkTeste(Integer value ,String desc) {
+        this.value =value;
+        this.desc =desc;
+    }
+
+    public  int value() {
+        return  this.value;
+    }
+
+    public static DrinkTeste valueOf(int code) {
+        DrinkTeste[] var1 = values();
+        int var2 = var1.length;
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            DrinkTeste status = var1[var3];
+            if (status.value == code) {
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException("No matching constant for [" + code + "]");
+    }
 }
