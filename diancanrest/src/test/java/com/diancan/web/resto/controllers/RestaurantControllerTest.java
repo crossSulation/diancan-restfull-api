b7f0005fe6910afc5040c7ed53e5366851e9786c
laxpuzzle <laxajaxmail@gmail.com>
2018-01-07 16:02:05 +0800

    add test

--- diancanrest/src/test/java/com/diancan/web/resto/controllers/RestaurantControllerTest.java
+++ diancanrest/src/test/java/com/diancan/web/resto/controllers/RestaurantControllerTest.java
@@ -7,12 +7,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.equalTo;
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest(RestaurantController.class)
+@AutoConfigureMockMvc
 public class RestaurantControllerTest {
 
     @Autowired
