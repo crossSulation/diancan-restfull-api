ac688fd4e596d6d0ca8f12904ee7f7e93a2c3f8f
Dumping ABI <laxajaxmail@gmail.com>
2018-01-12 18:04:29 +0800

    add eureka server

---
+++ daincan-eureka-server/src/test/java/com/daincan/daincaneurekaserver/DaincanEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daincan.daincaneurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaincanEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- diancanrest/src/main/java/com/diancan/Application.java
+++ diancanrest/src/main/java/com/diancan/Application.java
@@ -3,12 +3,12 @@ package com.diancan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class Application {
     public  static  void main(String ...args) {
         SpringApplication.run(Application.class,args);
